d7a75881aa8eba47df605faa2214c62826144d0f
dlfaican <dlfaican@espe.edu.ec>
2024-03-11 19:32:01 -0500

    Creacion

---
+++ src/main/groovy/ec/edu/espe/gestionproductos/GestionproductosApplication.groovy
@@ -0,0 +1,13 @@
+package ec.edu.espe.gestionproductos
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GestionproductosApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GestionproductosApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ec/edu/espe/gestionproductos/GestionproductosApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ec.edu.espe.gestionproductos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GestionproductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
